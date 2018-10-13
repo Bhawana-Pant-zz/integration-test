e2341879c4e14acfff6bd46aa36c7e31fae0f488
Bhawana Pant <bhawana.pant@esure.com>
2018-10-13 18:46:00 +0100

    minor fix to check if env is null or empty.

--- test-framework/src/main/groovy/com/esure/integrationtest/config/ConfigLoader.groovy
+++ test-framework/src/main/groovy/com/esure/integrationtest/config/ConfigLoader.groovy
@@ -12,10 +12,14 @@ class ConfigLoader {
     }
 
     private void loadConfig() {
-        def environment = System.getProperty('env')  == null ? 'dev' : System.getProperty('env')
+        def environment = isNullOrEmpty() ? 'dev' : System.getProperty('env')
         println "********************** Environment is set to $environment *************************"
         this.config = new ConfigSlurper()
             .parse(getClass().getResource('/config.groovy'))
             .get('env')[environment] as ConfigObject
+    }
+
+    private boolean isNullOrEmpty() {
+        System.getProperty('env') == null || System.getProperty('env').trim().isEmpty()
     }
 }
